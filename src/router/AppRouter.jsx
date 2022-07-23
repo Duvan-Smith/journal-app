2f6474f5b9dc3930ba24f72b703adbfb742b2d7c
DsmithMr <dsmith.mr@gmail.com>
2022-07-23 13:07:38 -0500

    Added CheckingAuth

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth";
 import { JournalRoutes } from "../journal";
+import { CheckingAuth } from "../ui";
+import { useSelector } from "react-redux";
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+
+  if (status === "checking") {
+    return <CheckingAuth />;
+  }
+
   return (
     <Routes>
       <Route path="/auth/*" element={<AuthRoutes />} />
