95d26907b4fc33675f9df3b858a606cf41394a20
DsmithMr <dsmith.mr@gmail.com>
2022-07-23 22:15:51 -0500

    Added animation

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -63,7 +63,10 @@ export const RegisterPage = () => {
   return (
     <AuthLayout title="Register">
       <h1> FormValid: {isFormValid ? "Valido" : "Incorrecto"} </h1>
-      <form onSubmit={onSubmit}>
+      <form
+        onSubmit={onSubmit}
+        className="animate__animated animate__fadeIn animate__faster"
+      >
         <Grid container>
           <Grid item xs={12} sx={{ mb: 2 }}>
             <TextField
