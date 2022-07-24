992b4e56b06b088c68f20ad79b352fab7901e117
DsmithMr <dsmith.mr@gmail.com>
2022-07-23 19:29:08 -0500

    Added validation to CheckingAuth

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,10 +2,24 @@ import { Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth";
 import { JournalRoutes } from "../journal";
 import { CheckingAuth } from "../ui";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { FirebaseAuth } from "../firebase/config";
+import { login, logout } from "../store/auth";
 
 export const AppRouter = () => {
   const { status } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      if (!user) return dispatch(logout());
+      const { uid, email, displayName, photoURL } = user;
+
+      dispatch(login({ uid, email, displayName, photoURL }));
+    });
+  }, []);
 
   if (status === "checking") {
     return <CheckingAuth />;
