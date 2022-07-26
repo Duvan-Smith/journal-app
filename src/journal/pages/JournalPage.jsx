c82b0e2bd64f7077c421155ea7b307ac264dc224
DsmithMr <dsmith.mr@gmail.com>
2022-07-25 19:12:34 -0500

    Added startNewNote

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,13 +2,22 @@ import { AddOutlined } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout";
 import { NoteView, NothingSelectedView } from "../views";
+import { useDispatch } from "react-redux";
+import { startNewNote } from "../../store/journal";
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote);
+  };
+
   return (
     <JournalLayout>
       <NothingSelectedView />
       <NoteView />
       <IconButton
+        onClick={onClickNewNote}
         size="large"
         sx={{
           color: "white",
